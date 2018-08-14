6bdfa59d7371132cf286746e8155bda03f0f3b27
Daniel Avelar <avelar42@hotmail.com>
2018-08-14 11:41:42 -0300

    Admin Pedidos Finalizados

--- vendor/hcodebr/php-classes/src/Model/OrderStatus.php
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -12,6 +12,14 @@ use \Hcode\Model;
 		const PAGO = 3;
 		const ENTREGUE = 4;
 
+		public static function listAll()
+		{
+			$sql = new Sql();
+			return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+		}
+
 	}
+
+	
 
  ?>
